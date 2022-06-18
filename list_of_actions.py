8ca438904bc50556aada17b563d2b53019ae9853
Stkos95 <plstkos@yandex.ru>
2022-06-18 15:14:52 +0300

    before changing personal handler

--- list_of_actions.py
+++ list_of_actions.py
@@ -6,48 +6,38 @@ import datas
 times = ("First half", "Second half")
 teams = ("Team one", "Team two")
 
-
-
 class Teams(NamedTuple):
     team_one: dict
     team_two: dict
 
 
-# ----------------------------------Для командной статистики-------------------
-# team_analytic = ('передача✅',
-#                  'передача❌',
-#                  'Прием мяча✅',
-#                  'Прием мяча❌',
-#                  'удар✅',
-#                  'удар❌',
-#                  'Обводка✅',
-#                  'Обводка❌',
-#                  'ЖК',
-#                  'КК',
-#                  '🛡Перехват✅',
-#                  '🛡Перехват❌',
-#                  '🛡Отбор✅',
-#                  '🛡Отбор❌',
-#                  'Угловые',
-#                  'Голевые моменты',
-#                  'Штрафные удары',
-#                  'Подборы',
-#                  'Потери')
 
-
+#-------------------Summary-----------------------------
 def act():
     return {action: 0 for action in datas.ACTIONS_FOR_KB}
 
+#---------------------Personal-------------------------
+
+def get_zone_actions():
+    return {i: 0 for i in datas.ACTIONS_FOR_PERSONAL}
+
+
+def get_other_actions():
+    return {i: 0 for i in datas.OTHER}
+
+
+def get_shoots_actions():
+    return {i: 0 for i in datas.SHOOTS}
 
 # def get_actions_for_counting():
 #     return Teams(team_one=act(), team_two=act())
 
 
-def get_dict_of_match_info(times, actions, teams=None) -> dict:
-    return {half: {team: {action: 0 for action in actions} for team in teams} for half in times}
-
-
-def get_dict_of_team_analytic(times: tuple, actions: tuple) -> dict:
-    return {half: {action: 0 for action in actions} for half in times}
+# def get_dict_of_match_info(times, actions, teams=None) -> dict:
+#     return {half: {team: {action: 0 for action in actions} for team in teams} for half in times}
+#
+#
+# def get_dict_of_team_analytic(times: tuple, actions: tuple) -> dict:
+#     return {half: {action: 0 for action in actions} for half in times}
 
 
